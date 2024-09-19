1d9611aae994650a860d9d94ce9d4550f92be7e7
Tiago Araújo <tiagodavi70@gmail.com>
2024-09-19 12:59:30 +0100

    exemplos i o

---
+++ entradasaida.py
@@ -0,0 +1,8 @@
+# soma com valores
+
+num1 = 2
+num2 = 5
+
+soma = num1 + num2
+
+print(soma)
---
+++ entradasaida_var.py
@@ -0,0 +1,8 @@
+# soma com variaveis de entrada
+
+num1 = int(input())
+num2 = int(input())
+
+soma = num1 + num2
+
+print(soma)
